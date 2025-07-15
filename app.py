17357a71b7264983fa752f63fe65a375cc589fad
Jasiek <u119_jangaw_waw@technischools.com>
2025-07-15 18:13:08 +0200

    15.07

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS
-import json
+import json, time
 
 app = Flask(__name__)
 CORS(app)
 socket = SocketIO(app, cors_allowed_origins="*")
 
-global track, step
+global track_name, step, steps_number, tracks, combination
 step = 0
+combination = []
 
 
-@socket.on("connect")
-def handle_connect():
-    socket.emit("server_message", {"data": "Jakimś cudem działa"})
+def open_file() -> list:
+    with open("./tracks.json", "r", encoding="utf-8") as file:
+        tracks = json.load(file)
+    return tracks
 
 
 @socket.on("login")
@@ -33,10 +35,8 @@ def login(data):
 
 @socket.on("get_tracks")
 def send_tracks(username):
-    with open("tracks.json", "r", encoding="utf-8") as file:
-        tracks = json.load(file)
-
     tracks_to_send = []
+    tracks = open_file()
 
     for track in tracks:
         owners = track.get("owners")
@@ -85,7 +85,7 @@ def sharable(username):
     sharable_users = []
 
     for user in users:
-        if user["name"] != username:
+        if user["name"] != username and user["name"] != "admin":
             sharable_users.append(user["name"])
 
     socket.emit("sharable", sharable_users)
@@ -93,8 +93,7 @@ def sharable(username):
 
 @socket.on("share")
 def share(data):
-    with open("./tracks.json", "r", encoding="utf-8") as file:
-        tracks = json.load(file)
+    tracks = open_file()
 
     for track in tracks:
         if track["name"] == data["track_name"]:
@@ -128,42 +127,58 @@ def share(data):
 
 @socket.on("select_track")
 def game_mode(data):
-    global track, step
+    global track_name, step, steps_number
+    tracks = open_file()
+
+    for track in tracks:
+        if track["name"] == data["track_name"]:
+            steps_number = track["steps"]
+
     step = 0
-    track = data["title"]
+    track_name = data["track_name"]
     socket.emit(
         "track_selected",
-        {
-            "success": True,
-            "title": track,
-        },
+        {"success": True, "title": track_name, "steps": steps_number},
     )
 
 
 @socket.on("start_playing")
 def start_playing():
-    global track
-    socket.emit(
-        "play",
-        {
-            "success": True,
-            "title": track,
-        },
-    )
+    global track_name, step, steps_number
+    tracks = open_file()
+    step = 1
+
+    for track in tracks:
+        if track["name"] == track_name:
+            socket.emit(
+                "play",
+                {
+                    "success": True,
+                    "title": track_name,
+                    "steps": f"{step}/{steps_number}",
+                    "combination": track["combination"][str(step)],
+                },
+            )
 
 
 @socket.on("next_step")
 def next_step():
-    global step
-    if step < 12:
+    global step, steps_number
+    tracks = open_file()
+
+    if step < steps_number:
         step += 1
-        socket.emit(
-            "next_step_info",
-            {
-                "success": True,
-                "steps": f"{step}/12",
-            },
-        )
+
+        for track in tracks:
+            if track["name"] == track_name:
+                socket.emit(
+                    "next_step_info",
+                    {
+                        "success": True,
+                        "steps": f"{step}/{steps_number}",
+                        "combination": track["combination"][str(step)],
+                    },
+                )
     else:
         socket.emit(
             "next_step_info",
@@ -175,15 +190,42 @@ def next_step():
 
 
 @socket.on("previoust_step")
-def previoust_step():
-    global step
-    if step > 0:
+def previoust_step(data=None):
+    global step, steps_number, track_name
+
+    tracks = open_file()
+    if data != None:
+        try:
+            step -= 1
+            for track in tracks:
+                if track["name"] == data["track_name"]:
+                    socket.emit(
+                        "previoust_step_info",
+                        {
+                            "success": True,
+                            "combination": track["combination"][
+                                str(data["step_to_edit"])
+                            ],
+                        },
+                    )
+        except:
+            print("except")
+            socket.emit(
+                "previoust_step_info",
+                {
+                    "success": False,
+                    "message": "Nie można przejść do poprzedniego kroku",
+                },
+            )
+
+    elif step > 1:
         step -= 1
+
         socket.emit(
             "previoust_step_info",
             {
                 "success": True,
-                "steps": f"{step}/12",
+                "steps": f"{step}/{steps_number}",
             },
         )
     else:
@@ -194,6 +236,136 @@ def previoust_step():
                 "message": "Nie można przejść do poprzedniego kroku",
             },
         )
+
+
+@socket.on("create_track")
+def track_create(data):
+    tracks = open_file()
+    for track in tracks:
+        if track["name"] == data["track_name"]:
+            socket.emit(
+                "creating_track_info",
+                {
+                    "success": False,
+                    "message": f"Utwór o tytule „{data['track_name']}” już istnieje",
+                },
+            )
+            return
+
+    new_track = {
+        "name": data["track_name"],
+        "owners": ["admin"],
+        "steps": 0,
+        "combination": {},
+    }
+
+    tracks.append(new_track)
+
+    with open("./tracks.json", "w", encoding="utf-8") as file:
+        json.dump(tracks, file, indent=2, ensure_ascii=False)
+
+    socket.emit(
+        "creating_track_info",
+        {
+            "success": True,
+            "message": f"Tytuł „{data['track_name']}” jest wolny",
+        },
+    )
+
+
+@socket.on("combination")
+def combination_add(data):
+    tracks = open_file()
+    try:
+        for track in tracks:
+            if track["name"] == data["track_name"]:
+                track["steps"] = 1 if track["steps"] == 0 else track["steps"] + 1
+                step = track["steps"]
+                owner = data["owner"]
+                actual_combination = data["active_cords"]
+                track["combination"].update({str(step): actual_combination})
+                if owner not in track["owners"]:
+                    track["owners"].append(owner)
+
+        with open("./tracks.json", "w", encoding="utf-8") as file:
+            json.dump(tracks, file, indent=2, ensure_ascii=False)
+
+        socket.emit(
+            "combination_creating_info",
+            {
+                "success": True,
+                "message": f"Krok dodany",
+            },
+        )
+    except:
+        socket.emit(
+            "combination_creating_info",
+            {
+                "success": False,
+                "message": f"Krok nie został dodany poprawnie",
+            },
+        )
+
+
+@socket.on("editing_combination")
+def combination_edit(data):
+    tracks = open_file()
+    try:
+        for track in tracks:
+            if track["name"] == data["track_name"]:
+                step = data["step"]
+                owner = data["owner"]
+                actual_combination = data["active_cords"]
+                track["combination"].update({str(step): actual_combination})
+                if owner not in track["owners"]:
+                    track["owners"].append(owner)
+
+        with open("./tracks.json", "w", encoding="utf-8") as file:
+            json.dump(tracks, file, indent=2, ensure_ascii=False)
+
+        socket.emit(
+            "combination_editing_info",
+            {
+                "success": True,
+                "message": f"Krok został edytowany",
+            },
+        )
+    except:
+        socket.emit(
+            "combination_editing_info",
+            {
+                "success": False,
+                "message": f"Krok nie został edytowany poprawnie",
+            },
+        )
+
+
+@socket.on("confirm_track")
+def confirm_track(data):
+    socket.emit("get_combination")
+    time.sleep(3)
+    global combination
+    tracks = open_file()
+
+    new_track = {
+        "name": data["track_name"],
+        "owners": [data["owner"]],
+        "steps": data["steps"],
+        "combination": combination,
+    }
+
+    tracks.append(new_track)
+
+    with open("./tracks.json", "w", encoding="utf-8") as file:
+        json.dump(tracks, file, indent=2, ensure_ascii=False)
+
+    socket.emit(
+        "track_created",
+        {
+            "success": True,
+            "message": f"Utwór „{data['track_name']}” został utworzony.",
+        },
+    )
 
 
 if __name__ == "__main__":
